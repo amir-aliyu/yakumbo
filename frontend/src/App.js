5201bd20c211937e234c9d442158180add827dff
ariost11 <ariostrow@gmail.com>
2024-03-24 17:47:27 -0400

    uploaded script from old (not working)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import Home from './components/Home/Home.tsx';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Home>
-        Test
-      </Home>
-    </div>
-  );
+	return (
+		<div className="App">
+			<head>
+				<title>Plant Project WebApp</title>
+			</head>
+			<body>
+				<Home/>
+			</body>
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -4,8 +4,50 @@ import React, { FC } from 'react';
 interface HomeProps {}
 
 const Home: FC<HomeProps> = () => (
-<div>
-	Home Component
+<div className="container">
+	<h1 className="mt-4 mb-4">Plant Watering App</h1>
+
+	{/* Add Plant Form */}
+	<div className="card mb-4">
+		<div className="card-header">
+			Add a Plant
+		</div>
+		<div className="card-body">
+			<form id="addPlantForm">
+				<div className="form-group">
+					<label for="plantName">Plant Name:</label>
+					<input type="text" className="form-control mt-2" id="plantName" placeholder="Enter plant name"
+						required/>
+				</div>
+				<div className="form-group mt-2">
+					<label for="plantType">Plant Type:</label>
+					<input type="text" className="form-control mt-2" id="plantType" placeholder="Enter plant type"
+						required/>
+				</div>
+				<div className="form-group mt-2">
+					<label for="wateringTime">Watering Time (in seconds):</label>
+					<input type="number" className="form-control mt-2" id="wateringTime"
+						placeholder="Enter watering time" required/>
+				</div>
+				<button type="submit" className="btn btn-primary mt-2">Add Plant</button>
+			</form>
+		</div>
+	</div>
+
+	{/* Notifications  */}
+	<div id="notifications"></div>
+
+	{/* Plant List */}
+	<div className="card mt-4">
+		<div className="card-header">
+			Plants List
+		</div>
+		<div className="card-body">
+			<ul id="plantsList" className="list-group">
+				{/* Plants will be dynamically added here */}
+			</ul>
+		</div>
+	</div>
 </div>
 );
 
