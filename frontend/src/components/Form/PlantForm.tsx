2e9c06c26d607d8577758e61bc43e16637a246a8
erikjearl <erikjearl@gmail.com>
2024-03-26 18:22:48 -0400

    move form into componenet and add edit plant button

---
+++ frontend/src/components/Form/PlantForm.tsx
@@ -0,0 +1,110 @@
+// PlantFormModal.tsx
+import React, { FC, useState, useEffect } from 'react';
+import './PlantForm.css';
+
+interface PlantFormProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (name: string, type: string, wateringTime: string) => Promise<void>;
+  plantData?: { name: string; type: string; wateringTime: string }; // Optional for edit
+}
+
+const PlantForm: FC<PlantFormProps> = ({ isOpen, onClose, onSubmit, plantData }) => {
+  const [plantName, setPlantName] = useState('');
+  const [plantType, setPlantType] = useState('');
+  const [wateringTime, setWateringTime] = useState('');
+
+  useEffect(() => {
+    if (plantData) {
+      setPlantName(plantData.name);
+      setPlantType(plantData.type);
+      setWateringTime(plantData.wateringTime);
+    } else {
+      setPlantName('');
+      setPlantType('');
+      setWateringTime('');
+    }
+  }, [plantData]);
+
+  const handleClose = () => {
+    onClose();
+    resetForm();
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    await onSubmit(plantName, plantType, wateringTime);
+    handleClose(); // Close the modal and reset the form on successful submission
+  };
+
+  if (!isOpen) return null; // Don't render the modal if it's not open
+
+  const resetForm = () => {
+    setPlantName('');
+    setPlantType('');
+    setWateringTime('');
+  };
+
+  return (
+    <div className="modal" style={{ display: 'block' }}>
+      {/* Overlay */}
+      <div className="modal-backdrop"></div>
+      {/* Modal Content */}
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{plantData ? 'Edit Plant' : 'Add New Plant'}</h5>
+            <button type="button" className="btn-close" onClick={handleClose}></button>
+          </div>
+          <div className="modal-body">
+            <form onSubmit={handleSubmit}>
+              
+            {/* Add Plant Form */}
+            <div className="form-group">
+              <label htmlFor="plantName">Plant Name:</label>
+              <input
+                type="text"
+                className="form-control mt-2"
+                id="plantName"
+                placeholder="Enter plant name"
+                value={plantName}
+                onChange={(e) => setPlantName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group mt-2">
+              <label htmlFor="plantType">Plant Type:</label>
+              <input
+                type="text"
+                className="form-control mt-2"
+                id="plantType"
+                placeholder="Enter plant type"
+                value={plantType}
+                onChange={(e) => setPlantType(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group mt-2">
+              <label htmlFor="wateringTime">Watering Time (in seconds):</label>
+              <input
+                type="number"
+                className="form-control mt-2"
+                id="wateringTime"
+                placeholder="Enter watering time"
+                value={wateringTime}
+                onChange={(e) => setWateringTime(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary mt-2">
+            {plantData ? 'Edit Plant' : 'Add New Plant'}
+            </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlantForm;
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,14 +1,14 @@
 import React, { FC, useState, useEffect, useCallback } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import PlantForm from '../Form/PlantForm.tsx';
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = () => {
   const [plants, setPlants] = useState<any[]>([]); // Adjusted for TypeScript
-  const [plantName, setPlantName] = useState('');
-  const [plantType, setPlantType] = useState('');
-  const [wateringTime, setWateringTime] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingPlant, setEditingPlant] = useState<{ _id: string; name: string; type: string; wateringTime: string } | null>(null);
 
   const displayNotification = (message: string, type: "success" | "error" | "info") => {
     toast[type](message);
@@ -26,35 +26,13 @@ const Home: FC<HomeProps> = () => {
         throw new Error(data.message || 'Error fetching plants');
       }
     } catch (error: any) {
-      displayNotification(error.message || 'Error fetching plants. Please try again.', 'error');
+      displayNotification('Error fetching plants', 'error');
     }
   }, []);
 
   useEffect(() => {
     fetchPlants();
   }, [fetchPlants]);
-
-  const addPlant = async (event: React.FormEvent) => {
-    event.preventDefault();
-    try {
-      const response = await fetch('/api/plants/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name: plantName, type: plantType, wateringTime: wateringTime }),
-      });
-      const data = await response.json();
-      if (response.ok) {
-        displayNotification('Plant added successfully!', 'success');
-        fetchPlants(); // Refresh the list
-      } else {
-        throw new Error(data.message || 'Failed to add plant');
-      }
-    } catch (error: any) {
-      displayNotification(error.message || 'Error adding plant. Please try again.', 'error');
-    }
-  };
 
   const deletePlant = async (plantId: string) => {
     try {
@@ -69,114 +47,150 @@ const Home: FC<HomeProps> = () => {
         throw new Error(data.message || 'Failed to delete plant');
       }
     } catch (error: any) {
-      displayNotification(error.message || 'Error deleting plant. Please try again.', 'error');
+      displayNotification('Error deleting plant', 'error');
     }
   };
 
+  // Function to open modal for adding a new plant
+  const handleAddPlantClick = () => {
+    setEditingPlant(null); // Ensure no plant data is set for adding
+    setIsModalOpen(true);
+  };
+
+  // Function to open modal for editing an existing plant
+const handleEditPlantClick = async (plantId: string) => {
+  try {
+    const response = await fetch(`/api/plants/${plantId}`);
+    const data = await response.json();
+    if (response.ok) {
+      setEditingPlant(data); // Set the current plant data to edit
+      setIsModalOpen(true);
+    } else {
+      throw new Error(data.message || 'Failed to fetch plant details');
+    }
+  } catch (error: any) {
+    displayNotification('Error fetching plant details', 'error');
+  }
+};
+
+  // Function to close the modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // Function to submit the form data
+  const handleSubmit = async (name: string, type: string, wateringTime: string) => {
+    if (editingPlant) {
+      // call the API to update the plant
+    try {
+      const response = await fetch(`/api/plants/${editingPlant._id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, type, wateringTime }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        displayNotification('Plant updated successfully!', 'success');
+        fetchPlants(); // Refresh the list
+      } else {
+        throw new Error(data.message || 'Failed to update plant');
+      }
+    } catch (error: any) {
+      displayNotification('Error updating plant', 'error');
+    }
+    } else {
+       // call api to add new plant
+      try {
+        const response = await fetch('/api/plants/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name, type, wateringTime }),
+        });
+        const data = await response.json();
+        if (response.ok) {
+          displayNotification('Plant added successfully!', 'success');
+          fetchPlants(); // Refresh the list
+          closeModal(); // Close the modal
+        } else {
+          throw new Error(data.message || 'Failed to add plant');
+        }
+      } catch (error: any) {
+        displayNotification('Error adding plant', 'error');
+      }
+    }
+    closeModal();
+  };
+  
+
   return (
     <div className="container">
-      <h1 className="mt-4 mb-4">Plant Watering App</h1>
+      <h1 className="mt-4 mb-4 fw-bold">Plant Watering App</h1>
 
-      {/* Add Plant Form */}
-      <div className="card mb-4">
-        <div className="card-header">Add a Plant</div>
-        <div className="card-body">
-          <form onSubmit={addPlant}>
-            <div className="form-group">
-              <label htmlFor="plantName">Plant Name:</label>
-              <input
-                type="text"
-                className="form-control mt-2"
-                id="plantName"
-                placeholder="Enter plant name"
-                value={plantName}
-                onChange={(e) => setPlantName(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group mt-2">
-              <label htmlFor="plantType">Plant Type:</label>
-              <input
-                type="text"
-                className="form-control mt-2"
-                id="plantType"
-                placeholder="Enter plant type"
-                value={plantType}
-                onChange={(e) => setPlantType(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-group mt-2">
-              <label htmlFor="wateringTime">Watering Time (in seconds):</label>
-              <input
-                type="number"
-                className="form-control mt-2"
-                id="wateringTime"
-                placeholder="Enter watering time"
-                value={wateringTime}
-                onChange={(e) => setWateringTime(e.target.value)}
-                required
-              />
-            </div>
-            <button type="submit" className="btn btn-primary mt-2">
-              Add Plant
-            </button>
-          </form>
-        </div>
+
+    {/* Plant List */}
+    <div className="card mt-4">
+      <div className="card-header fw-bold fs-4 d-flex align-items-center">
+        <p className="m-0">Plant List</p>
+        <button onClick={handleAddPlantClick} className="btn btn-primary ms-auto">Add Plant</button>
       </div>
-
-{/* Plant List */}
-<div className="card mt-4">
-  <div className="card-header">Plants List</div>
-  <div className="card-body">
-    <ul id="plantsList" className="list-group">
-      {/* Header Row */}
-      <li className="list-group-item">
-        <div className="row">
-          <div className="col fw-bold">Name</div>
-          <div className="col fw-bold">Type</div>
-          <div className="col fw-bold">Watering Time</div>
-          <div className="col fw-bold text-end">Actions</div> {/* Right-aligned header */}
-        </div>
-      </li>
-      {/* Plant Items */}
-      {plants.map((plant: any) => (
-        <li key={plant._id} className="list-group-item">
-          <div className="row">
-            <div className="col">
-              {plant.name}
+      <div className="card-body">
+        <ul id="plantsList" className="list-group">
+          {/* Header Row */}
+          <li className="list-group-item">
+            <div className="row">
+              <div className="col fw-bold fs-5">Name</div>
+              <div className="col fw-bold fs-5">Type</div>
+              <div className="col fw-bold fs-5">Watering Time</div>
+              <div className="col fw-bold fs-5 text-end">Actions</div> {/* Right-aligned header */}
             </div>
-            <div className="col">
-              {plant.type}
-            </div>
-            <div className="col">
-              {plant.wateringTime}s
-            </div>
-            <div className="col text-end">
-              <button
-                className="btn btn-info btn-sm me-2"
-                style={{ width: '75px' }}
-                onClick={() => deletePlant(plant._id)}
-              >
-                Edit
-              </button>
-              <button
-                className="btn btn-danger btn-sm"
-                style={{ width: '75px' }}
-                onClick={() => deletePlant(plant._id)}
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </div>
-</div>
+          </li>
+          {/* Plant Items */}
+          {plants.map((plant: any) => (
+            <li key={plant._id} className="list-group-item">
+              <div className="row">
+                <div className="col">
+                  {plant.name}
+                </div>
+                <div className="col">
+                  {plant.type}
+                </div>
+                <div className="col">
+                  {plant.wateringTime}s
+                </div>
+                <div className="col text-end">
+                  <button
+                    className="btn btn-info btn-sm me-2"
+                    style={{ width: '75px' }}
+                    onClick={() => handleEditPlantClick(plant._id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    style={{ width: '75px' }}
+                    onClick={() => deletePlant(plant._id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
 
-
-
+    {/* Plant Form Modal */}
+    <PlantForm
+      isOpen={isModalOpen}
+      onClose={closeModal}
+      onSubmit={handleSubmit}
+      plantData={editingPlant ? { name: editingPlant.name, type: editingPlant.type, wateringTime: editingPlant.wateringTime } : undefined}
+    />
 
     </div>
   );
