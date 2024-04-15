22506df62994de97fc1146d3d7797b3986b208c3
evelyndrake <evelyndrake90@gmail.com>
2024-04-15 18:06:05 -0400

    Actually added the test script, added test for DB connection

--- backend/tests.mjs
+++ backend/tests.mjs
@@ -1,11 +1,20 @@
 import assert from 'node:assert';
 import test from 'node:test';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 test('that 1 is equal 1', () => {
   assert.strictEqual(1, 1);
 });
 
-test('that throws as 1 is not equal 2', () => {
-  // throws an exception because 1 != 2
-  assert.strictEqual(1, 2);
+// Boolean for whether we are connected to the database
+let connected = false;
+
+test('Test if we are connected to the database', async () => {
+    await mongoose.connect(process.env.ATLAS_URI).then(() => {
+        connected = true;
+    });
+    assert.strictEqual(connected, true);
 });
