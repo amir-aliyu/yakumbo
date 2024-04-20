07de3bbb1c22268fc2e5cc86aa72f650d7f4b9e0
madscl <madeleine.clore@gmail.com>
2024-04-20 14:41:48 -0400

    Made perferences look pretty

--- frontend/src/components/Preferences/Preferences.tsx
+++ frontend/src/components/Preferences/Preferences.tsx
@@ -8,14 +8,29 @@ const Preferences = () => {
     };
 
     return (
-        <div>
-            <h1>My Preferences</h1>
+      <div className="container">
+        <h1 className="mt-4 mb-4 fw-bold">My Preferences</h1>
+        <div className="card mt-4 shadow">
+          <div className="card-header fw-bold d-flex align-items-center bg-primary text-white">
+            <p className="m-0 fs-3">Preferences</p>
+          </div>
+          <div className="card-body" style={{ backgroundColor: 'rgba(110, 187, 164, 0.4)' }}>
             <h2>Do you want to receive recipes?</h2>
             <select value={selection} onChange={handleChange}>
-                <option value="yes">Yes</option>
-                <option value="no">No</option>
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
             </select>
+          </div>
         </div>
+      </div>
+        // <div>
+        //     <h1>My Preferences</h1>
+        //     <h2>Do you want to receive recipes?</h2>
+        //     <select value={selection} onChange={handleChange}>
+        //         <option value="yes">Yes</option>
+        //         <option value="no">No</option>
+        //     </select>
+        // </div>
     );
 };
   
