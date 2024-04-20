42d9073b5c560fee7d827fe94e7ee2014c0ba901
madscl <madeleine.clore@gmail.com>
2024-04-20 14:43:16 -0400

    Made login look pretty

--- frontend/src/components/Preferences/Preferences.tsx
+++ frontend/src/components/Preferences/Preferences.tsx
@@ -23,14 +23,6 @@ const Preferences = () => {
           </div>
         </div>
       </div>
-        // <div>
-        //     <h1>My Preferences</h1>
-        //     <h2>Do you want to receive recipes?</h2>
-        //     <select value={selection} onChange={handleChange}>
-        //         <option value="yes">Yes</option>
-        //         <option value="no">No</option>
-        //     </select>
-        // </div>
     );
 };
   
