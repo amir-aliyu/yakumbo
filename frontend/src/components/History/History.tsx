87c2c1d9aecd269999b53a23b6ed6cf4e744d7e8
Ari <90860260+ariost11@users.noreply.github.com>
2024-04-24 13:59:27 -0400

    made table template for recipe history

--- frontend/src/components/History/History.tsx
+++ frontend/src/components/History/History.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const History = () => {
     // Sample data for the table
-    const friendsData = [
-        { id: 1, name: "Amir", profilePicture: "url_to_amir_image", dashboardLink: "/amir-dashboard", plantPoints: 100, wateringStreak: 5 },
-        { id: 2, name: "Jon", profilePicture: "url_to_jon_image", dashboardLink: "/jon-dashboard", plantPoints: 80, wateringStreak: 3 },
-        { id: 3, name: "Evelyn", profilePicture: "url_to_evelyn_image", dashboardLink: "/evelyn-dashboard", plantPoints: 120, wateringStreak: 7 }
-    ];
+    const [recipes, setRecipes] = useState<any[]>(
+    [
+        {
+            _id: 123,
+            name: "test",
+            recipeHtml: "test/test"
+        },
+        {
+            _id: 1234,
+                name: "test2",
+                recipeHtml: "test/test"
+        }
+    ]);
 
     return (
         <div className="container">
@@ -16,7 +24,34 @@ const History = () => {
                     <p className="m-0 fs-3">History</p>
                 </div>
                 <div className="card-body" style={{ backgroundColor: 'rgba(110, 187, 164, 0.4)' }}>
-                    <p>date - view recipe - have plant?</p>
+                    <ul id="plantsList" className="list-group">
+                        {/* Header Row */}
+                        <li className="list-group-item bg-light">
+                            <div className="row">
+                                <div className="col fw-bold fs-5">Recipe</div>
+                                <div className="col fw-bold fs-5 text-end">Plant Owned</div>
+                            </div>
+                        </li>
+                        {recipes.map((recipe: any) => (
+                            <li key={recipe._id} className="list-group-item">
+                                <div className="row">
+                                    <div className="col">
+                                        <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target={"#" + recipe.name}>
+                                            {recipe.name}
+                                        </button>
+                                    </div>
+                                    <div className="col text-end">
+                                        <input className="form-check-input" type="checkbox" checked disabled/>
+                                    </div>
+                                    <div className="collapse" id={recipe.name}>
+                                        <div className="card card-body">
+                                            LOAD HTML FILE HERE
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </div>
